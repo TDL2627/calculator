3ed0bd180546bf3aaae16ffbdaab1d2062e94604
TDL2627 <the.dark.lord.2627@gmail.com>
2021-11-25 10:57:29 +0200

    added things

--- script/script.js
+++ script/script.js
@@ -14,19 +14,3 @@ function clearSum(){
     sum="";
     document.getElementById("output").innerText=sum;
 }
-function light() {
-    document.getElementById("calculator").style.backgroundColor = "white";
-    let buttons = document.getElementsByClassName("numbut");
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].style.backgroundColor="grey";
-        buttons[i].style.color="white";
-    }
-  }
-  function dark() {
-    document.getElementById("calculator").style.backgroundColor = "black";
-    let buttons = document.getElementsByClassName("numbut");
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].style.backgroundColor="white";
-        buttons[i].style.color="black";
-    }
-  }
